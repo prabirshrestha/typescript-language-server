a78aeebafb83bbb2862afb2254a76e799ec81070
Prabir Shrestha <mail@prabir.me>
2017-08-19 19:17:33 +0000

    added commander for cli options

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,19 @@
 #!/usr/bin/env node
 
+import * as program from 'commander';
+
 import { Server } from './server';
+
+program
+    .version(require('../package.json').version)
+    .option('--stdio', 'use stdio')
+    .option('--node-ipc', 'use node-ipc')
+    .option('--socket', 'use socket. example: --socket=5000')
+    .parse(process.argv);
+
+if (!(program.stdio || program.socket || program['node-ipc'])) {
+    console.log('Connection type required (stdio, node-ipc, socket). Refer help for more details.');
+    process.exit(1);
+}
 
 new Server({}).listen();
